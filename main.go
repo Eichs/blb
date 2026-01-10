15e70b366078eb5342f219b6cbecb1431ba77736
zzz <88489522+Eichs@users.noreply.github.com>
2026-01-10 21:17:10 +0800

    实现blb修改打包

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"crypto/rand"
+	"fmt"
+	"log"
 	"os"
 )
 
@@ -13,7 +15,46 @@ func RandomHeaderKey16() []byte {
 	return key
 }
 func main() {
-	fh, err := os.Open("./20527480.blk")
+	ParseBlkFileInfo("./repacked.blk", false)
+
+}
+
+func ParseBlkFileInfo(filename string, isExtract bool) {
+	fh, err := os.Open(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer fh.Close()
+
+	b, err := ParseBlb3(fh)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if isExtract {
+		if err := b.ExtractAllToDir("out_unpack"); err != nil {
+			log.Fatal(err)
+		}
+	}
+	fmt.Printf("---- Blb3 Header ----\n")
+	fmt.Printf("HeaderSize            = %d (0x%X)\n", b.Header.HeaderSize, b.Header.HeaderSize)
+	fmt.Printf("LastUncompressedSize  = %d (0x%X)\n", b.Header.LastUncompressedSize, b.Header.LastUncompressedSize)
+	fmt.Printf("BlobOffset            = %d (0x%X)\n", b.Header.BlobOffset, uint32(b.Header.BlobOffset))
+	fmt.Printf("BlobSize              = %d (0x%X)\n", b.Header.BlobSize, b.Header.BlobSize)
+	fmt.Printf("CompressionType       = %d\n", b.Header.CompressionType)
+	fmt.Printf("BlockPow              = %d\n", b.Header.BlockPow)
+	fmt.Printf("BlockSize             = %d (0x%X)\n", b.Header.BlockSize, b.Header.BlockSize)
+	fmt.Printf("BlocksInfoCount       = %d\n", b.Header.BlocksInfoCount)
+	fmt.Printf("NodesCount            = %d\n", b.Header.NodesCount)
+	fmt.Printf("BlocksInfoOffsetAbs   = %d (0x%X)\n", b.Header.BlocksInfoOffsetAbs, b.Header.BlocksInfoOffsetAbs)
+	fmt.Printf("NodesInfoOffsetAbs    = %d (0x%X)\n", b.Header.NodesInfoOffsetAbs, b.Header.NodesInfoOffsetAbs)
+	fmt.Printf("FlagInfoOffsetAbs     = %d (0x%X)\n", b.Header.FlagInfoOffsetAbs, b.Header.FlagInfoOffsetAbs)
+	fmt.Printf("----------------------\n")
+
+}
+
+// RepackBlkFile xxx.blk CNRELWin https://sdk.yuanshen.org.cn/query_region_list 5
+func RepackBlkFile(filename, channelName, dispatchURL string, keyID int) {
+	fh, err := os.Open(filename)
 	if err != nil {
 		panic(err)
 	}
@@ -26,9 +67,9 @@ func main() {
 
 	for name, data := range cabs {
 		newData, err := PatchCABBytes(name, data, ConfigPatch{
-			ChannelName: "CNRELWin",
-			DispatchURL: "https://sdk.yuanshen.org.cn/query_region_list",
-			KeyID:       5,
+			ChannelName: channelName,
+			DispatchURL: dispatchURL,
+			KeyID:       keyID,
 		})
 		if err != nil {
 			panic(err)
